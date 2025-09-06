9f9df61d67b16b9d8d18e430a8d9e72af614360c
Jimmy <keepcreate06@gmail.com>
2025-09-06 10:16:18 +0800

    git init

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import TodoListView from '@/views/TodoListView.vue';
+import LoginView from '@/views/LoginView.vue';
+import RegisterView from '@/views/RegisterView.vue';
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/todolist',
+      name: 'todolist',
+      component: TodoListView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+  ],
+});
+
+export default router;
